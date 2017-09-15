b850907d1ae1e3b4b53ed1e7824721e9a5caf337
Andrea Bosco <boscoandrea@gmail.com>
2017-09-15 17:02:31 +0200

    Updates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,6 +20,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
@@ -220,6 +221,8 @@ class DefaultController extends Controller
     {
         
         $ciclos = null;
+
+        $groupedCiclos = null;
 
         $allCiclos = $this->getDoctrine()
         ->getRepository(Ciclo::class)
